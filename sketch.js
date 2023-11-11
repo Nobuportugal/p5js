03ab814ca2dae5dcd8db9963b2cc3be06dc2121f
NobuAI <48357605+Nobuportugal@users.noreply.github.com>
2023-11-11 23:26:55 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -72,9 +72,9 @@ function gotResult(error, results) {
   // The results are in an array ordered by confidence.
   // console.log(results[0&1]);
   label = results[0].label;
-  confidence = nf(result[0].confidence, 0, 2);
+  //confidence = nf(result[0].confidence, 0, 2);
   label2 = results[1].label;
-  confidence2 = nf(result[1].confidence, 0, 2);
+  //confidence2 = nf(result[1].confidence, 0, 2);
   // Classifiy again!
   classifyVideo();
 }
